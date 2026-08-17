135fe812cde147761444a2ee64be0625abd646b1
zafar-03 <zfnanshabhai@gmail.com>
2026-08-17 10:43:26 +0530

    Polymorphism: Method overloading & Method overriding issubclass() and super()

---
+++ OOP/Polymorphism.py
@@ -0,0 +1,65 @@
+# Polymorphism:   method/function  many forms
+
+# print(11)
+# print("Raj")
+# print((11,12,14))
+
+# class Calc:
+#     # def add(self,a,b):
+#     #     print(a+b)
+#     def add(self,a,b,c=0):
+#             print(a+b+c)
+
+# c1 = Calc()
+
+# c1.add(12,13)
+# c1.add(12,13,10)
+
+
+# class Person:
+#     def __init__(self):
+#         self.fname = "Raj"
+        
+#     def display(self):
+#         print("Display Person")
+
+
+# class Child(Person):
+#     def __init__(self):
+#         self.lname = "shah"
+#         super().__init__()
+
+#     def biodata(self):
+#         print(self.fname,self.lname)
+        
+#     def display(self):
+#         # super().display()
+#         print("Display Child")
+
+# c1 = Child()
+
+# c1.display()
+# c1.biodata()
+
+# print(issubclass(Child,Person))
+# print(issubclass(Person,Child))
+
+
+# class A :
+#     pass
+
+# class B(A):
+#     pass
+
+# class C(B):
+#     pass
+
+
+# print(issubclass(C,B))
+# print(issubclass(B,A))
+# print(issubclass(C,A))
+# print(issubclass(A,B))
+# print(issubclass(A,C))
+
+
+
