86d28090ce445973dbd7e577a224957672bb2a4d
zafar-03 <zfnanshabhai@gmail.com>
2026-08-26 10:52:22 +0530

    exception Handling

---
+++ last.py
@@ -0,0 +1,50 @@
+# num_1 =12
+# num_2 = 0
+
+
+# output = num_1/num_2   # infinite
+
+# print(output)
+num_1 =12
+num_2 = 0
+
+# try:            # Try Block 
+#     output = num_1/num_2
+# except:
+#     print("Error")
+# else:
+#     print(output)
+
+
+
+
+
+# try:            # Try Block 
+    # output = num_1/num_2
+    # print(data)
+    # print(int("Raj"))
+    # print("Raj" + 12)
+    # output = True - 3
+    # output = "12" * 2
+    # output = [12,13] + 1
+    # output = [14,12,13] - [14,15]
+    # output = (1,2) - (3,4)
+    # output = "Raj" * True
+
+# except ZeroDivisionError:
+#     print("division by zero")
+# except TypeError:
+#     print("Type Error")
+# except ValueError:
+#     print("Value Error")
+# except NameError:
+#     print("Variable Not Define")
+# else:
+#     print(output,type(output))
+
+
+# print(0.1 + 0.2)
+# print(0.5 + 0.25)
+
+
+# Bugs : memory : 32/64  : binary : 0/1 :   1971  : 2038  18 jul ,2038
