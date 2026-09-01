ad9708d01da6ac3d60373f37820a633426cfa203
zafar-03 <zfnanshabhai@gmail.com>
2026-09-01 10:52:18 +0530

    All Merged and Module Start

---
+++ Modules/calc.py
@@ -0,0 +1,32 @@
+# module
+
+def addition(n1,n2):
+    print(f"Addition of {n1} and {n2} : {n1+n2}")
+
+def subtraction(n1,n2):
+    print(f"Subtraction of {n1} and {n2} : {n1-n2}")
+
+def division(n1,n2):
+    print(f"Division of {n1} and {n2} : {n1/n2}")
+
+def multiplication(n1,n2):
+    print(f"Multiplication of {n1} and {n2} : {n1*n2}")
+
+
+
+# addition(5,3)
+
+# class XYZ:
+#     pass 
+
+
+PI = 3.14
+
+
+
+
+# print(__name__)
+
+
+# assert __name__ == "__main__"
+# addition(12,4)
---
+++ Modules/modules.py
@@ -0,0 +1,60 @@
+# module :
+"""
+Syntax : 
+1. import module_name
+
+
+2. import module_name as alias_name
+
+
+3. from module_name import funcationalities
+
+
+
+types : 
+1. Built-in  Module   (Pre-defined > Python install)
+2. User defined Module(custom Module)
+"""
+
+
+
+# import 
+# import calc    # connect (code)
+
+# calc.addition(12,13)
+# calc.multiplication(3,2)
+
+# print(calc.PI)
+
+
+
+# import calc as myCalculator   # connect :  run 
+
+# myCalculator.addition(12,13)
+# myCalculator.multiplication(3,2)
+
+# print(myCalculator.PI)
+
+
+# from calc import addition
+
+# addition(1,2)
+
+# ==================================
+# __name__ and __main__ : 
+
+"""
+1.dunder name : __name__   : global property :
+    1. __main__
+    2. module_name
+
+2.dunder main : __main__   : global property
+
+double underscore name
+
+"""
+
+# print(__name__)
+
+import calc
+
